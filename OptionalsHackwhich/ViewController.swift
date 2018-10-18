7d0b14f6feb6e97e0a2ea12782deb398604f8402
REGALADO-MORALES,ARM ADRIAN <rl106816@rlas-macair-22.rlas-116.org>
2018-10-18 12:07:27 -0500

    Initial Commit

---
+++ OptionalsHackwhich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OptionalsHackwhich
+//
+//  Created by REGALADO-MORALES,ARM ADRIAN on 10/18/18.
+//  Copyright © 2018 REGALADO-MORALES,ARM ADRIAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
